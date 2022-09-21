4b434f6f1de555bc6c2d7db0a2033e8362446754
karlarn <karlarnichols@gmail.com>
2022-09-21 13:07:42 -0500

    Added a model view for goal with updates

---
+++ GoalGetters/Models/GoalWithUpdatesView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace GoalGetters.Models
+{
+    public class GoalWithUpdatesView
+    {
+        public int Id { get; set; }
+        public int UserProfileId { get; set; }
+        [Required]
+        public string GoalToMeet { get; set; }
+        [Required]
+        public int DifficultyLevelId { get; set; }
+        public DateTime DateCreated { get; set; }
+        [Required]
+        public DateTime ExpectedCompletionDate { get; set; }
+        public string WorstCaseScenario { get; set; }
+        public DateTime CompletionDate { get; set; }
+        public DifficultyLevel DifficultyLevel { get; set; }
+        public UserProfile UserProfile { get; set; }
+        public List<GoalUpdate> GoalUpdates { get; set; }
+    }
+}
